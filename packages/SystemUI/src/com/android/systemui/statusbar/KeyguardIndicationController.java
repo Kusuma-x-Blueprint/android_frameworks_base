2a99b6c7a374c87acb5cc812db6fa4de5da8dd53
Pranav Vashi <neobuddy89@gmail.com>
2022-07-02 02:48:55 +0530

    SystemUI: Add current divider config for lockscreen charging
    * Some modern devices measure directly in mA instead uA.
    * We will let devices specify config_currentInfoDivider to check
      if uA to mA conversion is required.
    * Use same divider for wattage calculation.
    
    Change-Id: I7851cd682857cb69508e16ae0349a35e10d96788
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -203,6 +203,8 @@ public class KeyguardIndicationController {
     
     private boolean mFaceDetectionRunning;
 
+    private int mCurrentDivider;
+
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
     private boolean mDozing;
@@ -342,6 +344,8 @@ public class KeyguardIndicationController {
         mKeyguardStateController.addCallback(mKeyguardStateCallback);
 
         mStatusBarStateListener.onDozingChanged(mStatusBarStateController.isDozing());
+
+        mCurrentDivider = mContext.getResources().getInteger(R.integer.config_currentInfoDivider);
     }
 
     public void setIndicationArea(ViewGroup indicationArea) {
@@ -521,7 +525,7 @@ public class KeyguardIndicationController {
         if (mPowerPluggedIn || mEnableBatteryDefender) {
             String powerIndication = computePowerIndication();
             if (DEBUG_CHARGING_SPEED) {
-                powerIndication += ",  " + (mChargingWattage / 1000) + " mW";
+                powerIndication += ",  " + (mChargingWattage / mCurrentDivider) + " mW";
             }
 
             mKeyguardLogger.logUpdateBatteryIndication(powerIndication, mPowerPluggedIn);
@@ -974,11 +978,11 @@ public class KeyguardIndicationController {
 
         String batteryInfo = "";
         if (mChargingCurrent > 0) {
-            batteryInfo = batteryInfo + (mChargingCurrent / 1000) + "mA";
+            batteryInfo = batteryInfo + (mChargingCurrent / mCurrentDivider) + "mA";
         }
         if (mChargingWattage > 0) {
             batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                    String.format("%.1f" , (mChargingWattage / 1000 / 1000)) + "W";
+                    String.format("%.1f" , (mChargingWattage / mCurrentDivider / 1000)) + "W";
         }
         if (mChargingVoltage > 0) {
             batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
